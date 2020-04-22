577ded097cef357f24fd5a488b7c6bde9bec0dbb
Odysseas Georgoudis <odygrd@gmail.com>
2020-04-22 10:09:00 +0100

    add a spinlock test and also improve the spinlock

--- quill/quill/src/detail/misc/Spinlock.cpp
+++ quill/quill/src/detail/misc/Spinlock.cpp
@@ -7,29 +7,19 @@ namespace detail
 /***/
 void Spinlock::lock() noexcept
 {
-  while (_spinlock_flag.load(std::memory_order_relaxed) == State::Locked)
+  do
   {
-    // keep trying
-  }
-
-  // here we can try to acquire the lock
-  State old_state = _spinlock_flag.exchange(State::Locked, std::memory_order_acquire);
-
-  while (old_state == State::Locked)
-  {
-    // We failed to acquire the lock
     while (_spinlock_flag.load(std::memory_order_relaxed) == State::Locked)
     {
       // keep trying
     }
-    old_state = _spinlock_flag.exchange(State::Locked, std::memory_order_acquire);
-  }
+  } while (_spinlock_flag.exchange(State::Locked, std::memory_order_acquire) == State::Locked);
 }
 
 /***/
 bool Spinlock::try_lock() noexcept
 {
-  // we want 0 to be returned meaning the lock was previously free and invert it to return true that lock was acquired
+  // if we acquired the lock with exchange() that returns FREE -> 0 , and we invert to return true
   return _spinlock_flag.load(std::memory_order_relaxed) == State::Free
     ? !_spinlock_flag.exchange(State::Locked, std::memory_order_acquire)
     : false;
