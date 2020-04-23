36d7a13af150a45cf61724d0ae3c0d763692047e
Odysseas Georgoudis <odygrd@gmail.com>
2020-04-23 01:25:41 +0100

    experimenting with arm64 build ..

--- quill/quill/include/quill/detail/misc/Rdtsc.h
+++ quill/quill/include/quill/detail/misc/Rdtsc.h
@@ -1,3 +1,8 @@
+/**
+ * Copyright(c) 2020-present, Odysseas Georgoudis & quill contributors.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
 #pragma once
 
 #include "quill/detail/misc/Attributes.h"
@@ -11,10 +16,6 @@ namespace detail
 // arm64
 QUILL_NODISCARD_ALWAYS_INLINE_HOT uint64_t rdtsc() noexcept
 {
-  // System timer of ARMv8 runs at a different frequency than the CPU's.
-  // The frequency is fixed, typically in the range 1-50MHz.  It can be
-  // read at CNTFRQ special register.  We assume the OS has set up
-  // the virtual timer properly.
   int64_t virtual_timer_value;
   asm volatile("mrs %0, cntvct_el0" : "=r"(virtual_timer_value));
   return virtual_timer_value;
@@ -29,18 +30,14 @@ QUILL_NODISCARD_ALWAYS_INLINE_HOT uint64_t rdtsc() noexcept
   uint32_t pmccntr;
   uint32_t pmuseren;
   uint32_t pmcntenset;
-  // Read the user mode perf monitor counter access permissions.
+
   asm volatile("mrc p15, 0, %0, c9, c14, 0" : "=r"(pmuseren));
   if (pmuseren & 1)
   {
-    // Allows reading perfmon counters for user mode code.
     asm volatile("mrc p15, 0, %0, c9, c12, 1" : "=r"(pmcntenset));
     if (pmcntenset & 0x80000000ul)
     {
-      // Is it counting?
       asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
-
-      // The counter is set up to count every 64th cycle
       return (static_cast<uint64_t>(pmccntr)) * 64u;
     }
   }
@@ -50,7 +47,6 @@ QUILL_NODISCARD_ALWAYS_INLINE_HOT uint64_t rdtsc() noexcept
   gettimeofday(&tv, NULL);
   return static_cast<uint64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
 }
-
 #else
   // assume x86-64 ..
   #if defined(_WIN32)
