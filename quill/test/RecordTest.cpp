b1db858d091cec681580cee57e3754f3f04580ae
Odysseas Georgoudis <odygrd@gmail.com>
2020-05-08 02:48:07 +0100

    support string_views

--- quill/test/RecordTest.cpp
+++ quill/test/RecordTest.cpp
@@ -46,20 +46,4 @@ TEST(Record, construct)
     static_assert(std::is_same<decltype(msg)::PromotedTupleT, std::tuple<int, double, std::string>>::value,
                   "tuple is not promoted");
   }
-
-#if defined(QUILL_USE_STRING_VIEW)
-  {
-    // test with std::string_view the tuple get's promoted
-    using record_t = LogRecord<mock_log_record_info, int, std_string_view<char>, char const*>;
-    static_assert(std::is_same<record_t::PromotedTupleT, std::tuple<int, std::string, std::string>>::value,
-                  "tuple is not promoted");
-
-    // Try to contruct one using the same args
-    record_t msg{&logger_details, 1337, std_string_view<char>{"13.5"}, "test"};
-
-    // Check that the constructed msg has a promoted underlying tuple
-    static_assert(std::is_same<decltype(msg)::PromotedTupleT, std::tuple<int, std::string, std::string>>::value,
-                  "tuple is not promoted");
-  }
-#endif
 }
