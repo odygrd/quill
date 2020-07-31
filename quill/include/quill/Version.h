a1c0229541f5c9be9a06fc47218363b5e197eddb
Odysseas.Georgoudis <odygrd@gmail.com>
2020-07-31 20:23:38 +0300

    Update version in header file

--- quill/include/quill/Version.h
+++ quill/include/quill/Version.h
@@ -7,7 +7,7 @@
 
 #define QUILL_VERSION_MAJOR 1
 #define QUILL_VERSION_MINOR 3
-#define QUILL_VERSION_PATCH 2
+#define QUILL_VERSION_PATCH 3
 
 #define QUILL_VERSION                                                                              \
   (QUILL_VERSION_MAJOR * 10000 + QUILL_VERSION_MINOR * 100 + QUILL_VERSION_PATCH)
