fb229fbc3335248047f6f3eea4676aa164878e9a
Odysseas Georgoudis <odygrd@gmail.com>
2020-10-19 16:39:20 +0300

    Filters enchancement

--- quill/include/quill/handlers/Handler.h
+++ quill/include/quill/handlers/Handler.h
@@ -5,10 +5,12 @@
 
 #pragma once
 
-#include "quill/FilterBase.h"
 #include "quill/Fmt.h"
 #include "quill/PatternFormatter.h"
 #include "quill/detail/events/LogRecordMetadata.h"
+#include "quill/detail/misc/Common.h"
+#include "quill/detail/misc/RecursiveSpinlock.h"
+#include "quill/filters/LogLevelFilter.h"
 #include <algorithm>
 #include <memory>
 #include <vector>
@@ -74,12 +76,83 @@ public:
   QUILL_ATTRIBUTE_HOT virtual void flush() noexcept = 0;
 
   /**
+   * Sets a log level filter on the handler. Log statements with higher or equal severity only will be loged
+   * @param log_level the log level severity
+   */
+  void set_log_level(LogLevel log_level)
+  {
+    std::lock_guard<detail::RecursiveSpinlock> const lock(_global_filters_lock);
+    auto search_log_level_filter_it =
+      std::find_if(_global_filters.begin(), _global_filters.end(), [](std::unique_ptr<FilterBase>& filter) {
+        return filter->get_filter_name() == LogLevelFilter::filter_name;
+      });
+
+    if (search_log_level_filter_it != _global_filters.end())
+    {
+      // Just update the existing
+      auto* log_level_filter = reinterpret_cast<LogLevelFilter*>(search_log_level_filter_it->get());
+      log_level_filter->set_log_level(log_level);
+    }
+    else
+    {
+      // construct a new filter and add it to global
+      auto log_level_filter = std::make_unique<LogLevelFilter>(log_level);
+      add_filter(std::move(log_level_filter));
+    }
+  }
+
+  /**
+   * Looks up the existing log level filter that was set by set_log_level and returns the current log level
+   * @return the current log level of the log level filter - if any
+   */
+  QUILL_NODISCARD LogLevel get_log_level() noexcept
+  {
+    // When set_log_level is called for the first time, it won't exist in _local_filters
+    // until apply_filters has run at least once
+
+    auto const search_log_level_filter_it =
+      std::find_if(_local_filters.cbegin(), _local_filters.cend(), [](FilterBase const* filter) {
+        return filter->get_filter_name() == LogLevelFilter::filter_name;
+      });
+
+    if (search_log_level_filter_it != _local_filters.cend())
+    {
+      return reinterpret_cast<LogLevelFilter*>(*search_log_level_filter_it)->get_log_level();
+    }
+    else
+    {
+      // No filter found
+      return LogLevel::TraceL3;
+    }
+  }
+
+  /** Filters **/
+
+  /**
    * Adds a new filter for this handler
    * @param filter instance of a filter class as unique ptr
    */
-  void add_filter(std::unique_ptr<FilterBase> filter) noexcept
+  void add_filter(std::unique_ptr<FilterBase> filter)
   {
-    _filters.push_back(std::move(filter));
+    // Lock and add this filter to our global collection
+    std::lock_guard<detail::RecursiveSpinlock> const lock(_global_filters_lock);
+
+    // Check if the same filter already exists
+    auto const search_filter_it =
+      std::find_if(_global_filters.cbegin(), _global_filters.cend(),
+                   [&filter](std::unique_ptr<FilterBase> const& elem_filter) {
+                     return elem_filter->get_filter_name() == filter->get_filter_name();
+                   });
+
+    if (QUILL_UNLIKELY(search_filter_it != _global_filters.cend()))
+    {
+      QUILL_THROW(QuillError{"Filter with the same name already exists"});
+    }
+
+    _global_filters.push_back(std::move(filter));
+
+    // Indicate a new filter was added - here relaxed is okay as the spinlock will do acq-rel on destruction
+    _new_filter.store(true, std::memory_order_relaxed);
   }
 
   /**
@@ -88,11 +161,27 @@ public:
    */
   QUILL_NODISCARD bool apply_filters(char const* thread_id, std::chrono::nanoseconds log_record_timestamp,
                                      detail::LogRecordMetadata const& metadata,
-                                     fmt::memory_buffer const& formatted_record) noexcept
+                                     fmt::memory_buffer const& formatted_record)
   {
+    // Update our local collection of the filters
+    if (QUILL_UNLIKELY(_new_filter.load(std::memory_order_relaxed)))
+    {
+      // if there is a new filter we have to update
+      _local_filters.clear();
+
+      std::lock_guard<detail::RecursiveSpinlock> const lock(_global_filters_lock);
+      for (auto const& filter : _global_filters)
+      {
+        _local_filters.push_back(filter.get());
+      }
+
+      // all filters loaded so change to false
+      _new_filter.store(false, std::memory_order_relaxed);
+    }
+
     return std::all_of(
-      _filters.begin(), _filters.end(),
-      [thread_id, log_record_timestamp, &metadata, &formatted_record](std::unique_ptr<FilterBase>& filter_elem) {
+      _local_filters.begin(), _local_filters.end(),
+      [thread_id, log_record_timestamp, &metadata, &formatted_record](FilterBase* filter_elem) {
         return filter_elem->filter(thread_id, log_record_timestamp, metadata, formatted_record);
       });
   }
@@ -102,8 +191,15 @@ private:
    * must not be moved or copied. We create the default pattern formatter always on init */
   std::unique_ptr<PatternFormatter> _formatter{std::make_unique<PatternFormatter>()};
 
-  /** Filters for this handler **/
-  std::vector<std::unique_ptr<FilterBase>> _filters;
+  /** Local Filters for this handler **/
+  std::vector<FilterBase*> _local_filters;
+
+  /** Global filter for this handler - protected by a spinlock **/
+  std::vector<std::unique_ptr<FilterBase>> _global_filters;
+  quill::detail::RecursiveSpinlock _global_filters_lock;
+
+  /** Indicator that a new filter was added **/
+  alignas(detail::CACHELINE_SIZE) std::atomic<bool> _new_filter{false};
 };
 
 } // namespace quill
