56865f4b8b80e15a5b5440f68bf58d3c073025af
Odysseas Georgoudis <odygrd@gmail.com>
2021-04-17 11:38:43 +0300

    UAdd missing header needed for recent versions of fmt

--- quill/include/quill/Fmt.h
+++ quill/include/quill/Fmt.h
@@ -22,3 +22,14 @@
   #include "quill/bundled/fmt/ostream.h"
   #include "quill/bundled/fmt/ranges.h"
 #endif
+
+/**
+ * Also include additional files due to recent fmt versions
+ */
+#if FMT_VERSION > 70103
+  #if defined(QUILL_FMT_EXTERNAL)
+    #include <fmt/args.h>
+  #else
+    #include "quill/bundled/fmt/args.h"
+  #endif
+#endif
