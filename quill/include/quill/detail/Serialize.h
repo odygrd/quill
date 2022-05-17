09bc5ccd5e6037c7ed0a4688177a5fea0b9d9cc1
Odysseas Georgoudis <odygrd@gmail.com>
2022-05-17 09:56:27 +0100

    Update Serialize.h

--- quill/include/quill/detail/Serialize.h
+++ quill/include/quill/detail/Serialize.h
@@ -122,11 +122,11 @@ QUILL_NODISCARD_ALWAYS_INLINE_HOT constexpr size_t get_args_sizes(size_t* c_stri
   {
     size_t const len = strlen(arg) + 1;
     c_string_sizes[CstringIdx] = len;
-    return alignof(Arg) + len + get_args_sizes<CstringIdx + 1>(c_string_sizes, args...);
+    return len + get_args_sizes<CstringIdx + 1>(c_string_sizes, args...);
   }
   else if constexpr (is_type_of_string<Arg>())
   {
-    return alignof(Arg) + (arg.size() + 1) + get_args_sizes<CstringIdx>(c_string_sizes, args...);
+    return (arg.size() + 1) + get_args_sizes<CstringIdx>(c_string_sizes, args...);
   }
   else
   {
