af7d6b0033f553dcea579807abc7a7828e7c5831
Odysseas Georgoudis <odygrd@gmail.com>
2022-11-25 17:06:39 +0000

    reuse transit event objects in the backend logging thread

--- quill/include/quill/detail/backend/BackendWorker.h
+++ quill/include/quill/detail/backend/BackendWorker.h
@@ -752,7 +752,7 @@ TransitEvent* BackendWorker::_get_transit_event()
 
   auto* transit_event = _transit_event_factory.back();
   _transit_event_factory.pop_back();
-  transit_event->flush_flag->store(false);
+  transit_event->flush_flag = nullptr;
   return transit_event;
 }
 } // namespace quill::detail
