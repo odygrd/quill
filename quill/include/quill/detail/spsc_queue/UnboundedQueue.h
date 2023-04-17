309243d4d11af6d06c0649b68b277853b54acb75
Odysseas Georgoudis <odygrd@gmail.com>
2023-04-17 22:32:20 +0100

    throw when the max queue capacity is reached

--- quill/include/quill/detail/spsc_queue/UnboundedQueue.h
+++ quill/include/quill/detail/spsc_queue/UnboundedQueue.h
@@ -101,9 +101,14 @@ public:
       capacity = capacity * 2ull;
     }
 
-    if (QUILL_UNLIKELY(capacity > std::numeric_limits<uint32_t>::max()))
+    // bounded queue max power of 2 capacity since uint32_t type is used to hold the value 2147483648 bytes
+    constexpr uint64_t max_bounded_queue_capacity = (std::numeric_limits<uint32_t>::max() >> 1) + 1;
+    if (QUILL_UNLIKELY(capacity > max_bounded_queue_capacity))
     {
-      capacity = std::numeric_limits<uint32_t>::max();
+      QUILL_THROW(QuillError{
+        "logging single messages greater than 2 GB is not supported. nbytes: " + std::to_string(nbytes) +
+        " capacity: " + std::to_string(capacity) +
+        " max_bounded_queue_capacity: " + std::to_string(max_bounded_queue_capacity)});
     }
 
     // commit previous write to the old queue before switching
