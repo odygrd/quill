8df8f2ec20fd4ff1eb51ffbbd5e125108329f4fc
Tetsuya Hayashi <tetsu.h@gmail.com>
2023-07-23 10:50:36 +0900

    fix build failure of Intel Compiler Classic
    Intel Compiler Classic itself doesn't have x86gprintrin.h
    and it seems incompatible with GCC's x86gprintrin.h

--- quill/include/quill/detail/misc/Rdtsc.h
+++ quill/include/quill/detail/misc/Rdtsc.h
@@ -15,7 +15,7 @@
   // assume x86-64 ..
   #if defined(_WIN32)
     #include <intrin.h>
-  #elif (defined(__GNUC__) && __GNUC__ > 10) || (defined(__clang_major__) && __clang_major__ > 11)
+  #elif ((defined(__GNUC__) && __GNUC__ > 10) && !defined(__INTEL_COMPILER)) || (defined(__clang_major__) && __clang_major__ > 11)
     #include <x86gprintrin.h>
   #else
     // older compiler versions do not have <x86gprintrin.h>
