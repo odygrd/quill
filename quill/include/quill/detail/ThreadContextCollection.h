b356340885104d74e929201f0183ffe673e4ca57
Odysseas Georgoudis <odygrd@gmail.com>
2023-11-27 16:47:31 +0000

    improve compile time (#377)

--- quill/include/quill/detail/ThreadContextCollection.h
+++ quill/include/quill/detail/ThreadContextCollection.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "quill/Config.h"
+#include "quill/detail/ThreadContext.h"
 #include "quill/detail/misc/Attributes.h" // for QUILL_ATTRIBUTE_HOT
 #include "quill/detail/misc/Common.h"     // for CACHE_LINE_ALIGNED
 #include <atomic>                         // for atomic
@@ -22,8 +23,6 @@ struct Config;
 
 namespace detail
 {
-
-class ThreadContext;
 
 /**
  * ThreadContextCollection class
@@ -161,26 +160,7 @@ public:
    * If there are no invalidated contexts or no new contexts the existing cache is returned
    * @return All current owned thread contexts
    */
-  QUILL_NODISCARD QUILL_ATTRIBUTE_HOT backend_thread_contexts_cache_t const& backend_thread_contexts_cache()
-  {
-    // Check if _thread_contexts has changed. This can happen only when a new thread context is added by any Logger
-    if (QUILL_UNLIKELY(_has_new_thread_context()))
-    {
-      // if the thread _thread_contexts was changed we lock and remake our reference cache
-      std::lock_guard<std::mutex> const lock {_mutex};
-      _thread_context_cache.clear();
-
-      // Remake thread context ref
-      for (auto const& elem : _thread_contexts)
-      {
-        // We do skip invalidated && empty queue thread contexts as this is very rare, so instead
-        // we just add them and expect them to be cleaned in the next iteration
-        _thread_context_cache.push_back(elem.get());
-      }
-    }
-
-    return _thread_context_cache;
-  }
+  QUILL_NODISCARD QUILL_ATTRIBUTE_HOT backend_thread_contexts_cache_t const& backend_thread_contexts_cache();
 
   /**
    * Clears thread context cache from invalid and empty thread contexts
@@ -200,60 +180,29 @@ private:
    * @note Only accessed by the backend thread
    * @return true if the shared data structure was changed by any calls to Logger
    */
-  QUILL_NODISCARD QUILL_ATTRIBUTE_HOT bool _has_new_thread_context() noexcept
-  {
-    // Again relaxed memory model as in case it is false we will acquire the lock
-    if (_new_thread_context.load(std::memory_order_relaxed))
-    {
-      // if the variable was updated to true, set it to false,
-      // There should not be any race condition here as this is the only place _changed is set to
-      // false, and we will return true anyway
-      _new_thread_context.store(false, std::memory_order_relaxed);
-      return true;
-    }
-    return false;
-  }
+  QUILL_NODISCARD QUILL_ATTRIBUTE_HOT bool _has_new_thread_context() noexcept;
 
   /**
    * Indicate that the context has changed. A new thread context has been added or removed
    * @note Only called by the caller threads
    */
-  void _set_new_thread_context() noexcept
-  {
-    // Set changed is used with the lock, we can have relaxed memory order here as the lock
-    // is acq/rel anyway
-    return _new_thread_context.store(true, std::memory_order_relaxed);
-  }
+  void _set_new_thread_context() noexcept;
 
   /**
    * Increment the counter for a removed thread context. This notifies the backend thread to look for an invalidated context
    */
-  void _add_invalid_thread_context() noexcept
-  {
-    // relaxed is fine, see _has_invalid_thread_context explanation
-    _invalid_thread_context.fetch_add(1, std::memory_order_relaxed);
-  }
+  void _add_invalid_thread_context() noexcept;
 
   /**
    * Reduce the value of thread context removed counter. This is decreased by the backend thread
    * when we found and removed the invalided context
    */
-  void _sub_invalid_thread_context() noexcept
-  {
-    // relaxed is fine, see _has_invalid_thread_context explanation
-    _invalid_thread_context.fetch_sub(1, std::memory_order_relaxed);
-  }
+  void _sub_invalid_thread_context() noexcept;
 
   /**
    * @return True if there is an invalid thread context
    */
-  QUILL_NODISCARD QUILL_ATTRIBUTE_HOT bool _has_invalid_thread_context() const noexcept
-  {
-    // Here we do relaxed because if the value is not zero we will look inside ThreadContext invalid
-    // flag that is also a relaxed atomic, and then we will look into the SPSC queue size that is
-    // also atomic Even if we don't read everything in order we will check again in the next circle
-    return _invalid_thread_context.load(std::memory_order_relaxed) != 0;
-  }
+  QUILL_NODISCARD QUILL_ATTRIBUTE_HOT bool _has_invalid_thread_context() const noexcept;
 
   /**
    * Remove a thread context from our main thread context collection
