c71830423efd30a90a3d99da6fecf02c57ef7d06
weissle <2016301500298@whu.edu.cn>
2024-02-21 22:15:47 +0800

    Fix core dump when flush while the spsc queue doesn't have enough space.

--- quill/test/LogTest.cpp
+++ quill/test/LogTest.cpp
@@ -2473,4 +2473,37 @@ TEST_CASE("default_logger_with_very_large_random_strings")
   quill::detail::remove_file(filename);
 }
 
+TEST_CASE("flush_without_any_log")
+{
+  LogManager lm;
+
+  quill::Config cfg;
+
+  lm.configure(cfg);
+
+  lm.start_backend_worker(false, std::initializer_list<int32_t>{});
+
+  Logger* default_logger = lm.logger_collection().get_logger();
+
+  std::atomic_bool is_flush_done = false;
+
+  std::thread watcher(
+    [&is_flush_done]()
+    {
+      int try_count = 3;
+      while (is_flush_done.load() == false && try_count--)
+      {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+      }
+      REQUIRE_EQ(is_flush_done.load(), true);
+    });
+
+  lm.flush();
+
+  is_flush_done = true;
+
+  watcher.join();
+
+  lm.stop_backend_worker();
+}
 TEST_SUITE_END();
