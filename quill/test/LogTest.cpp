6c591396755a5ddd3b2f6b238447368c74913801
Odysseas Georgoudis <odygrd@gmail.com>
2024-03-15 21:05:51 +0000

    minor

--- quill/test/LogTest.cpp
+++ quill/test/LogTest.cpp
@@ -2476,22 +2476,16 @@ TEST_CASE("default_logger_with_very_large_random_strings")
 TEST_CASE("flush_without_any_log")
 {
   LogManager lm;
-
-  quill::Config cfg;
-
-  lm.configure(cfg);
-
   lm.start_backend_worker(false, std::initializer_list<int32_t>{});
 
   Logger* default_logger = lm.logger_collection().get_logger();
-
-  std::atomic_bool is_flush_done = false;
+  std::atomic<bool> is_flush_done = false;
 
   std::thread watcher(
     [&is_flush_done]()
     {
       uint32_t try_count = 2000;
-      while (is_flush_done.load() == false && try_count--)
+      while (!is_flush_done.load() && try_count--)
       {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
       }
@@ -2507,7 +2501,6 @@ TEST_CASE("flush_without_any_log")
 
   frontend.join();
   watcher.join();
-
   lm.stop_backend_worker();
 }
 TEST_SUITE_END();
