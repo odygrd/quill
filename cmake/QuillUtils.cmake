4c1fb36d70d34eb3b327f0489d4e750340d0e915
odygrd <odygrd@gmail.com>
2025-06-05 14:12:35 +0100

    add clang-cl

--- cmake/QuillUtils.cmake
+++ cmake/QuillUtils.cmake
@@ -72,15 +72,20 @@ function(set_common_compile_options target_name)
     )
 
     if (QUILL_NO_EXCEPTIONS)
+        # First, remove any default /EHsc flags for MSVC
+        if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+            target_compile_options(${target_name} ${COMPILE_OPTIONS_VISIBILITY} -/EHsc)
+        endif ()
+
         # Add flags -fno-exceptions -fno-rtti to make sure we support them
         target_compile_options(${target_name} ${COMPILE_OPTIONS_VISIBILITY}
                 $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
                 -fno-exceptions -fno-rtti>
                 $<$<CXX_COMPILER_ID:MSVC>:/wd4702 /GR- /EHs-c- /D_HAS_EXCEPTIONS=0>)
     else ()
-        # Additional MSVC specific options
-        if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-            target_compile_options(${target_name} ${COMPILE_OPTIONS_VISIBILITY} /EHsc)
-        endif ()
+        # Additional MSVC specific options - only set for non-QUILL_NO_EXCEPTIONS builds
+        target_compile_options(${target_name} ${COMPILE_OPTIONS_VISIBILITY}
+                $<$<CXX_COMPILER_ID:MSVC>:/EHsc>)
     endif ()
 endfunction()
+
