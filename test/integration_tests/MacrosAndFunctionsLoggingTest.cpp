dca86dab4e94fdfbe74909e4cc076423abbde06e
nankendrik <nunnk7392@gmail.com>
2026-08-26 03:24:03 +0300

    Add runtime log level to macro-free mode

--- test/integration_tests/MacrosAndFunctionsLoggingTest.cpp
+++ test/integration_tests/MacrosAndFunctionsLoggingTest.cpp
@@ -41,7 +41,11 @@ TEST_CASE("macros_and_functions_logging_test")
     }(),
     FileEventNotifier{});
 
-  Logger* logger = Frontend::create_or_get_logger(logger_name, std::move(file_sink));
+  Logger* logger = Frontend::create_or_get_logger(
+    logger_name, std::move(file_sink),
+    PatternFormatterOptions{
+      "%(time) [%(thread_id)] %(short_source_location:<28) LOG_%(log_level:<9) "
+      "%(logger:<12) %(message) [%(tags)]"});
 
   logger->set_log_level(LogLevel::Debug);
 
@@ -53,6 +57,15 @@ TEST_CASE("macros_and_functions_logging_test")
     error(logger, "This is message {}", i);
   }
 
+  LogLevel runtime_log_level{LogLevel::Notice};
+  size_t const runtime_log_line = __LINE__ + 1;
+  quill::log(logger, runtime_log_level, "Runtime message {}", number_of_messages);
+
+  std::string const tag{"RUNTIME_TAG"};
+  runtime_log_level = LogLevel::Critical;
+  size_t const tagged_runtime_log_line = __LINE__ + 1;
+  quill::log(logger, runtime_log_level, Tags{tag.c_str()}, "Tagged runtime message {}", 42);
+
   logger->flush_log();
   Frontend::remove_logger(logger);
 
@@ -61,7 +74,7 @@ TEST_CASE("macros_and_functions_logging_test")
 
   // Read file and check
   std::vector<std::string> const file_contents = quill::testing::file_contents(filename);
-  REQUIRE_EQ(file_contents.size(), number_of_messages * 4);
+  REQUIRE_EQ(file_contents.size(), (number_of_messages * 4) + 2);
 
   for (size_t i = 0; i < number_of_messages; ++i)
   {
@@ -78,6 +91,16 @@ TEST_CASE("macros_and_functions_logging_test")
     expected_string = "LOG_ERROR     " + logger_name + "       This is message " + std::to_string(i);
     REQUIRE(quill::testing::file_contains(file_contents, expected_string));
   }
+
+  REQUIRE(quill::testing::file_contains(
+    file_contents,
+    std::string{"MacrosAndFunctionsLoggingTest.cpp:"} + std::to_string(runtime_log_line) +
+      " LOG_NOTICE    " + logger_name + "       Runtime message " + std::to_string(number_of_messages)));
+
+  REQUIRE(quill::testing::file_contains(
+    file_contents,
+    std::string{"MacrosAndFunctionsLoggingTest.cpp:"} + std::to_string(tagged_runtime_log_line) +
+      " LOG_CRITICAL  " + logger_name + "       Tagged runtime message 42 [#RUNTIME_TAG ]"));
 
   testing::remove_file(filename);
 }
