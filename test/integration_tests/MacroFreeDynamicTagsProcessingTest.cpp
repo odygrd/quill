9a899e9fe8cdc10cd014fd516d6578cbd779ef04
HyunjinX917 <hyunjinpark319@tutamail.com>
2026-08-29 20:29:29 +0300

    Improve pattern formatter and release docs

--- test/integration_tests/MacroFreeDynamicTagsProcessingTest.cpp
+++ test/integration_tests/MacroFreeDynamicTagsProcessingTest.cpp
@@ -4,6 +4,7 @@
 #include "quill/Backend.h"
 #include "quill/Frontend.h"
 #include "quill/LogFunctions.h"
+#include "quill/LogMacros.h"
 #include "quill/sinks/FileSink.h"
 
 #include <cstdio>
@@ -15,7 +16,7 @@ using namespace quill;
 
 namespace
 {
-std::string process_execution_tags(char const* tags)
+std::string replace_executor_tag(char const* tags, std::string_view replacement)
 {
   std::string processed_tags{tags};
   constexpr std::string_view executor_id{"executor_17"};
@@ -23,10 +24,20 @@ std::string process_execution_tags(char const* tags)
   size_t const executor_id_pos = processed_tags.find(executor_id);
   if (executor_id_pos != std::string::npos)
   {
-    processed_tags.replace(executor_id_pos, executor_id.size(), "executor_render");
+    processed_tags.replace(executor_id_pos, executor_id.size(), replacement.data(), replacement.size());
   }
 
   return processed_tags;
+}
+
+std::string process_execution_tags(char const* tags)
+{
+  return replace_executor_tag(tags, "executor_render");
+}
+
+std::string process_sink_tags(char const* tags)
+{
+  return replace_executor_tag(tags, "executor_sink");
 }
 } // namespace
 
@@ -34,6 +45,8 @@ std::string process_execution_tags(char const* tags)
 TEST_CASE("macro_free_dynamic_tags_processing")
 {
   static constexpr char const* filename = "macro_free_dynamic_tags_processing.log";
+  static constexpr char const* override_filename =
+    "macro_free_dynamic_tags_processing_override.log";
   static constexpr char const* logger_name = "macro_free_dynamic_tags_processing_logger";
 
   Backend::start();
@@ -48,19 +61,34 @@ TEST_CASE("macro_free_dynamic_tags_processing")
     }(),
     FileEventNotifier{});
 
+  auto override_file_sink = Frontend::create_or_get_sink<FileSink>(
+    override_filename,
+    []()
+    {
+      FileSinkConfig cfg;
+      cfg.set_open_mode('w');
+
+      PatternFormatterOptions override_formatter_options{"[sink] %(tags)%(message)"};
+      override_formatter_options.process_tags = process_sink_tags;
+      cfg.set_override_pattern_formatter_options(override_formatter_options);
+      return cfg;
+    }(),
+    FileEventNotifier{});
+
   PatternFormatterOptions formatter_options{"%(tags)%(message)"};
   formatter_options.process_tags = process_execution_tags;
 
-  Logger* logger = Frontend::create_or_get_logger(logger_name, std::move(file_sink), formatter_options);
+  Logger* logger = Frontend::create_or_get_logger(
+    logger_name, {std::move(file_sink), std::move(override_file_sink)}, formatter_options);
 
-  size_t constexpr number_of_messages{9};
+  size_t constexpr dynamic_messages{9};
 
   {
     std::string const executor_tag{"executor_17"};
     std::string const priority_storage{"priority_1_not_part_of_the_tag"};
     std::string_view const priority_tag{priority_storage.data(), std::string_view{"priority_1"}.size()};
 
-    for (size_t i = 0; i < number_of_messages; ++i)
+    for (size_t i = 0; i < dynamic_messages; ++i)
     {
       if ((i % 3) == 0)
       {
@@ -77,14 +105,17 @@ TEST_CASE("macro_free_dynamic_tags_processing")
     }
   }
 
+  LOG_INFO_TAGS(logger, TAGS("executor_17"), "static macro tag");
+  info(logger, Tags{}, "empty tags");
+
   logger->flush_log();
   Frontend::remove_logger(logger);
   Backend::stop();
 
   std::vector<std::string> const file_contents = testing::file_contents(filename);
-  REQUIRE_EQ(file_contents.size(), number_of_messages);
+  REQUIRE_EQ(file_contents.size(), dynamic_messages + 2);
 
-  for (size_t i = 0; i < number_of_messages; ++i)
+  for (size_t i = 0; i < dynamic_messages; ++i)
   {
     if ((i % 3) == 0)
     {
@@ -100,5 +131,15 @@ TEST_CASE("macro_free_dynamic_tags_processing")
     }
   }
 
+  REQUIRE_EQ(file_contents[dynamic_messages], "#executor_render static macro tag");
+  REQUIRE_EQ(file_contents[dynamic_messages + 1], "empty tags");
+
+  std::vector<std::string> const override_file_contents = testing::file_contents(override_filename);
+  REQUIRE_EQ(override_file_contents.size(), dynamic_messages + 2);
+  REQUIRE(testing::file_contains(override_file_contents, "[sink] #executor_sink std::string tag 0"));
+  REQUIRE(testing::file_contains(override_file_contents, "[sink] #executor_sink static macro tag"));
+  REQUIRE(testing::file_contains(override_file_contents, "[sink] empty tags"));
+
   testing::remove_file(filename);
+  testing::remove_file(override_filename);
 }
